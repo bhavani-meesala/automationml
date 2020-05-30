62ac97a881851b0347365061b8d7b1c90bb6b274
bhavani-meesala <59745309+bhavani-meesala@users.noreply.github.com>
2020-05-31 00:30:19 +0530

    Add files via upload

---
+++ mnistdigit.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+
+# In[8]:
+
+
+x_train.shape
+
+
+# In[9]:
+
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+# Making sure that the values are float so that we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+# Normalizing the RGB codes by dividing it to the max RGB value.
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print('Number of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
+
+
+# In[10]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+# Creating a Sequential Model and adding the layers
+model = Sequential()
+model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten()) 
+model.add(Dense(128, activation=tf.nn.relu))
+model.add(Dropout(0.2))
+model.add(Dense(10,activation=tf.nn.softmax))
+model.summary()
+
+
+# In[16]:
+
+
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy', 
+              metrics=['accuracy'])
+trained=model.fit(x=x_train,y=y_train, epochs=1)
+
+
+# In[17]:
+
+
+model.evaluate(x_test, y_test)
+
+
+# In[18]:
+
+
+image_index = 4444
+plt.imshow(x_test[image_index].reshape(28, 28),cmap='Greys')
+pred = model.predict(x_test[image_index].reshape(1, img_rows, img_cols, 1))
+print(pred.argmax())
+
+
+# In[22]:
+
+
+acc=trained.history['accuracy'][-1]*100
+
+
+# In[23]:
+
+
+accur=int(acc)
+f=open("accuracy.txt",'w')
+f.write("{}".format(accur))
+f.close()
+
+
+# In[ ]:
+
+
+
+
